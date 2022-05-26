03ccaeda2f374baee32a7838cc3c2c9ddb6c5118
Youri Dautrebande <100094409+YouriDau@users.noreply.github.com>
2022-05-26 14:47:16 +0200

    Dernier jet

--- ProjetPNDI/main.c
+++ ProjetPNDI/main.c
@@ -9,15 +9,18 @@ void createFilesSet(char fileName[]);
 void createListingSubjects(Person subjects[]);
 void browseFolder(char path[][LG_PATHS], Person subjects[]);
 int browseFile(Person subjects[], int index, char folder[], FileSet fileSelected);
-void writeLine(FileSet file, bool lastLine);
+void writeLine(FileSet file, FILE* fi, bool lastLine);
 // PHASE 3
 void createFileModel(char fileName[]);
-void readData(char file[], Model model[]);
-void insertData(char file[], Model model[]);
+void readData(char file[], Model models[]);
+void insertData(char file[], Model models[]);
+// PHASE 4
+void estimateTypeOfMovement(Model models[]);
+double distanceEuclidienne(double data[], Model models[], int iClass);
 
 void main(void) {
 	Person subjects[NB_SUBJECTS];
-	Model model[NB_CLASSES];
+	Model models[NB_CLASSES];
 
 	char paths[NB_FOLDERS_MAX][LG_PATHS] = {
 		"dws_1/sub_",
@@ -38,24 +41,23 @@ void main(void) {
 	};
 
 	// PHASE 1
-	//createFilesSet("trainSet.csv");
-	//createFilesSet("testSet.csv");
-	//createListingSubjects(subjects);
-	//browseFolder(paths, subjects);
-
-	// PHASE 2
-	int estimatedClasses[] = { 1, 3, 4, 2, 5, 6, 2, 1};
-	int realClassesV[] = { 1, 3, 4, 2, 5, 6, 2, 1};
-	int realClasses[] = { 1, 4, 5, 2, 3, 6, 1, 1 };
-	//displayResultsByClass(realClasses, estimatedClasses, 8);
-	//displayAccuracy(realClasses, estimatedClasses, 8);
-	//displayConfusionMatrix(realClasses, estimatedClasses, 8);
+	createFilesSet("trainSet.csv");
+	createFilesSet("testSet.csv");
+	createListingSubjects(subjects);
+	browseFolder(paths, subjects);
+	
 
 	// PHASE 3
-	createFileModel("fiModel.csv");
-	readData("trainSet.csv", model);
-	insertData("fiModel.csv", model);
+	createFileModel("fiModel.csv"); 
+	readData("trainSet.csv", models);
+	insertData("fiModel.csv", models);
+
+	// PHASE 4
+	estimateTypeOfMovement(models);
+	
 }
+
+// Phase 1
 
 void createFilesSet(char fileName[]) {
 	FILE* fiFile;
@@ -118,7 +120,6 @@ void createListingSubjects(Person subjects[]) {
 void browseFolder(char paths[][LG_PATHS], Person subjects[]) {
 	char folder[LG_PATHS];
 	char typeMovement[4] = "";
-	char movement[LG_MOVEMENT];
 	FileSet fileSelected;
 	int index = 0;
 
@@ -157,9 +158,7 @@ int browseFile(Person subjects[], int index, char folder[], FileSet fileSelected
 	int iSubjectForTest = 0;
 	char iSubjectStr[3];
 
-	for (int iSubject = 1; iSubject <= NB_SUBJECTS; iSubject++) {  // PASSER DANS LES 24 FICHIERS
-		char file[LG_PATHS];
-
+	for (int iSubject = 2; iSubject <= NB_SUBJECTS; iSubject++) {  // PASSER DANS LES 24 FICHIERS
 		if (nbSubjectTest < NB_FILES_TEST) {
 			strcpy_s(fileSelected.type, LG_FOLER_TYPE, "testSet.csv");
 			nbSubjectTest++;
@@ -167,6 +166,7 @@ int browseFile(Person subjects[], int index, char folder[], FileSet fileSelected
 		else {
 			strcpy_s(fileSelected.type, LG_FOLER_TYPE, "trainSet.csv");
 		}
+		char file[LG_PATHS];
 
 		strcpy_s(file, LG_PATHS, folder);
 		sprintf(iSubjectStr, "%d", iSubject);
@@ -183,75 +183,68 @@ int browseFile(Person subjects[], int index, char folder[], FileSet fileSelected
 			puts("Error opening of file!");
 		}
 		else {
-			char line[800];
-			double infos[13];
-			double racine;
-			char* token;
-			int iLine = 0;
-			int iInfo;
-			Data data;
+			
+			FILE* fiTrainSet;
+			FILE* fiTestSet;
 
-			fgets(line, sizeof(line), fiMovement);
-			while (fgets(line, sizeof(line), fiMovement) && iLine < NB_DATA) {
-				iInfo = 0;
-
-				token = strtok(line, ",");
-				while (token != NULL) {
-					sscanf(token, "%.5lf", &infos[iInfo]);
-
-					token = strtok(NULL, ",");
-					iInfo++;
-				}
-
-				data.x = infos[10];
-				data.y = infos[11];
-				data.z = infos[12];
-
-				fileSelected.vacc = sqrt(pow(data.x, 2) + pow(data.y, 2) + pow(data.z, 2));
-				//fileSelected.vacc = racine;
-
-				writeLine(fileSelected, iLine == 0);
-
-				iLine++;
-			}
-
-			FILE* fiFile;
-			fopen_s(&fiFile, fileSelected.type, "a");
-			if (fiFile == NULL) {
-				puts("Error opening of file!");
+			fopen_s(&fiTrainSet, "trainSet.csv", "a");
+			fopen_s(&fiTestSet, "testSet.csv", "a");
+			if (fiTrainSet == NULL || fiTestSet == NULL) {
+				puts("Error opening trainSet or testSet of file!");
 			}
 			else {
-				fprintf(fiFile, "\n");
+				char line[800];
+				int iLine = 0;
+				Data data;
+				double trash;
+				bool fiIsTrain;
 
-				fclose(fiFile);
+				fgets(line, sizeof(line), fiMovement);
+				while (iLine < NB_DATA && !feof(fiMovement)) {
+					fgets(line, sizeof(line), fiMovement);
+
+					sscanf(line, "%lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf", &trash, &trash, &trash, &trash, &trash, &trash, &trash, &trash, &trash, &trash, &data.x, &data.y, &data.z);
+
+					fileSelected.vacc = sqrt(pow(data.x, 2) + pow(data.y, 2) + pow(data.z, 2));
+
+					fiIsTrain = strcmp(fileSelected.type, "trainSet.csv") == 0;
+
+					if (fiIsTrain) {
+						writeLine(fileSelected, fiTrainSet, iLine == 0);
+					} else {
+						writeLine(fileSelected, fiTestSet, iLine == 0);
+					}
+					iLine++;
+					fgets(line, sizeof(line), fiMovement);
+				}
+				if (fiIsTrain) {
+					fprintf(fiTrainSet, "%s", "\n");
+				} else {
+					fprintf(fiTestSet, "%s", "\n");
+				}
+
+				iSubjectForTest += 4;
+				if (iSubjectForTest >= NB_SUBJECTS) {
+					iSubjectForTest = 0;
+				}
+
+				fclose(fiTestSet);
+				fclose(fiTrainSet);
 			}
-
-			iSubjectForTest += 4;
-			if (iSubjectForTest >= NB_SUBJECTS) {
-				iSubjectForTest = 0;
-			}
-
 			fclose(fiMovement);
 		}
+	
 		index++;
 	}
 	return index;
 }
 
-void writeLine(FileSet file, bool firstLine) {
-	FILE* fiFile;
-	fopen_s(&fiFile, file.type, "a");
-	if (fiFile == NULL) {
-		puts("Error opening of file!");
+void writeLine(FileSet file, FILE* fi, bool firstLine) {
+	if (firstLine) {
+		fprintf(fi, "%s,%d,%d,", file.movement, file.index, file.gender);
 	}
-	else {
-		if (firstLine) {
-			fprintf(fiFile, "%s,%d,%d,", file.movement, file.index, file.gender);
-		}
-		fprintf(fiFile, "%.5lf,", file.vacc);
 
-		fclose(fiFile);
-	}
+	fprintf(fi, "%.5f,", file.vacc);
 }
 
 //phase 3
@@ -275,7 +268,8 @@ void createFileModel(char fileName[]) {
 	}
 }
 
-void readData(char file[], Model model[]) {
+// Changement par rapport au da car le code ne permet peut-être pas de stocker toutes les valeurs dans un tableau
+void readData(char file[], Model models[]) {
 	FILE* fiSet;
 
 	fopen_s(&fiSet, file, "r");
@@ -283,103 +277,271 @@ void readData(char file[], Model model[]) {
 		printf("Error opening of file!");
 	}
 	else {
-		char line[200];
+		char line[6000];
+		double data[600];
+		double sumTotal;
 		int iMovement;
-		int iTotalColumn;
-		int iColumn = 0;
-		int nbLines = 0;
-		double totalAverages = 0;
-		double totalValues = 0;
-		double totalColumns[NB_DATA];
-		double totalColumnsSquare[NB_DATA];
-		char movements[NB_CLASSES][LG_MOVEMENT] = { "downstairs", "jogging", "sitDown", "standUp", "upstairs", "walking" };
+		int nbLines[NB_CLASSES];
+		char movement[LG_MOVEMENT];
 
-		fgets(line, sizeof(line), fiSet);	// Ne pas prendre en compte la premi�re ligne
-		while (fgets(line, sizeof(line), fiSet)) {
-			iMovement = 0;
-			iColumn = 3;
 
-			if (iMovement < NB_CLASSES && line[0] != movements[iMovement]) {
-				iTotalColumn = 0;
-				while (iTotalColumn < NB_DATA) {
-					model[iMovement].averages[iTotalColumn] += (totalColumns[iTotalColumn] / nbLines) * 100;
-					totalAverages += model[iMovement].averages[iTotalColumn];
-					model[iMovement].standardDeviation[iTotalColumn] = sqrt((totalColumnsSquare[iTotalColumn] / nbLines)
-						- pow(model[iMovement].averages[iTotalColumn], 2));
+		// Init the arrays at 0
+		for (int i = 0; i < NB_DATA; i++) {
+			data[i] = 0;
+		}
+		for (int iMovement = 0; iMovement < NB_CLASSES; iMovement++) {
+			nbLines[iMovement] = 0;
 
-					iTotalColumn++;
-				}
-				model[iMovement].average = totalValues / ((double)nbLines * NB_DATA);
-
-				totalValues = 0;
-				iMovement++;
+			for (int iData = 0; iData < NB_DATA; iData++) {
+				models[iMovement].averages[iData] = 0;
+				models[iMovement].standardDeviation[iData] = 0;
 			}
-			else {
-				iTotalColumn = 0;
-				while (iTotalColumn < NB_DATA) {
-					totalColumns[iTotalColumn] += line[iColumn];
-					totalColumnsSquare[iTotalColumn] += pow(line[iColumn], 2);
-					totalValues += line[iColumn];
+			models[iMovement].average = 0;
+		}
 
-					iTotalColumn++;
-					iColumn++;
-				}
-				iTotalColumn = 0;
+		fgets(line, sizeof(line), fiSet);	// Ne pas prendre en compte la première ligne
+		while (fgets(line, sizeof(line), fiSet)) {
+			char* token;
+			int iData = 0;
+			
+			token = strtok(line, ",");
+			// Récupérer le type de movements
+			sscanf(token, "%s", &movement);
+			//puts(movement);
+
+			// passe 2 colonnes 
+			token = strtok(NULL, ",");
+			token = strtok(NULL, ",");
+			// Récupérer les VACC
+			token = strtok(NULL, ",");
+			while (token != NULL) {
+				sscanf(token, "%lf", &data[iData]);
+				token = strtok(NULL, ",");
+				iData++;
+			}
+
+			// trouver l'id du mouvement
+			if (strcmp(movement, "downstairs") == 0) {
+				iMovement = 0;
+			}
+			else if (strcmp(movement, "jogging") == 0) {
+				iMovement = 1;
+			}
+			else if (strcmp(movement, "sitDown") == 0) {
+				iMovement = 2;
+			}
+			else if (strcmp(movement, "standUp") == 0) {
+				iMovement = 3;
+			}
+			else if (strcmp(movement, "upstairs") == 0) {
+				iMovement = 4;
+			}
+			else if (strcmp(movement, "walking") == 0) {
+				iMovement = 5;
+			}
+			
+			nbLines[iMovement]++;
+
+			// Faire les sommes pour la moyenne et std
+			for (int iData = 0; iData < NB_DATA; iData++) {
+				models[iMovement].averages[iData] += data[iData];
+				models[iMovement].standardDeviation[iData] += pow(data[iData], 2);
 			}
 		}
 
-		fclose(&fiSet);
+		fclose(fiSet);
+		for (int iMovement = 0; iMovement < NB_CLASSES; iMovement++) {
+			sumTotal = 0;
+			for (int iData = 0; iData < NB_DATA; iData++) {
+				sumTotal += models[iMovement].averages[iData];
+				models[iMovement].averages[iData] /= nbLines[iMovement];
+				models[iMovement].standardDeviation[iData] = sqrt((models[iMovement].standardDeviation[iData] / nbLines[iMovement]) -
+														pow(models[iMovement].averages[iData], 2));
+			}
+			models[iMovement].average = sumTotal / ((double)nbLines[iMovement] * NB_DATA);
+		}
+		
 	}
 }
 
-//fonction qui � pour but de transf�rer les donn�es du tableau model et de les �crire dans le fichier
-void insertData(char file[], Model model[]) {
-	FILE* fiSet;
+//fonction qui à pour but de transférer les données du tableau models et de les écrire dans le fichier
+void insertData(char file[], Model models[]) {
+	FILE* fiModel;
 
-	fopen(&fiSet, file, "r");
-	if (fiSet == NULL) {
+	fopen_s(&fiModel, file, "a");
+	if (fiModel == NULL) {
 		printf("Error opening of file!");
-	}
-	else {
-		int iMovement = 0;
-		int nbMovementMax = 6;
-		int nbLineByMovementMax = 3;
-		char line[200];
+	} else {
+		char line[800];
 
-		fgets(line, sizeof(line), fiSet); //on �vitre la premi�re ligne
+		fgets(line, sizeof(line), fiModel); //on évitre la première ligne
 
-		while (iMovement < nbMovementMax)
-		{
-			int iMovementLine = 0;
+		for (int iMovement = 0; iMovement < NB_CLASSES; iMovement++) {
 
-			model[iMovement].movement[0] = MOVEMENTS[iMovement];
-			while (iMovementLine < nbLineByMovementMax)
-			{
-				int i = 0;
-
-				fprintf(fiSet, "%s,", model[iMovementLine].movement);
-
-				//affiche en fonction de la ligne
-				switch (iMovementLine)
-				{
-				case 0:
-					while (i < NB_DATA)
-					{
-						fprintf(fiSet, "%.5lf,", model[iMovementLine].averages[i]);
-					}
-				case 1:
-					while (i < NB_DATA) {
-						fprintf(fiSet, "%.5lf,", model[iMovementLine].standardDeviation[i]);
-					}
-				case 2:
-					fprintf(fiSet, "%.5lf,", model[iMovementLine].average);
-				}
-
-				iMovementLine++;
+			fprintf(fiModel, "%s,", MOVEMENTS[iMovement]);
+			for (int i = 0; i < NB_DATA; i++) {
+				fprintf(fiModel, "%.5f,", models[iMovement].averages[i]);
 			}
-			iMovement++;
+			fprintf(fiModel, "%s", "\n");
+
+			fprintf(fiModel, "%s,", MOVEMENTS[iMovement]);
+			for (int i = 0; i < NB_DATA; i++) {
+				fprintf(fiModel, "%.5f,", models[iMovement].standardDeviation[i]);
+			}
+			fprintf(fiModel, "%s", "\n");
+
+			fprintf(fiModel, "%s,", MOVEMENTS[iMovement]);
+			fprintf(fiModel, "%.5f,", models[iMovement].average);
+			fprintf(fiModel, "%s", "\n");
+
+			printf("Movement_%d    Writed\n", iMovement);
 		}
 
-		fclose(&fiSet);
+		fclose(fiModel);
 	}
+}
+
+
+//phase 4
+void estimateTypeOfMovement(Model models[]) {
+	int nbTests = 0;
+	FILE* fiTest;
+
+	fopen_s(&fiTest, "testSet.csv", "r");
+	if (fiTest == NULL) {
+		printf("Error opening of file!");
+	} else {
+		int realClasses[600];
+		int estimatedClasses[600];
+		char line[6000];
+		int iData;
+
+		fgets(line, sizeof(line), fiTest); //on évitre la première ligne
+		while (fgets(line, sizeof(line), fiTest))
+		{
+
+			char* token;
+			int iData = 0;
+			char movement[LG_MOVEMENT];
+			double data[600];
+
+			token = strtok(line, ",");
+			// Récupérer le type de movements
+			sscanf(token, "%s", &movement);
+			//puts(movement);
+
+			// passe 2 colonnes 
+			token = strtok(NULL, ",");
+			token = strtok(NULL, ",");
+			// Récupérer les VACC
+			token = strtok(NULL, ",");
+			while (token != NULL) {
+				sscanf(token, "%lf", &data[iData]);
+				token = strtok(NULL, ",");
+				iData++;
+			}
+
+			double sum = 0;
+			double correctAvg = 0;
+			double indicators[3];
+
+			if (strcmp(movement, "downstairs") == 0) {
+				realClasses[nbTests] = 1;
+			}
+			else if (strcmp(movement, "jogging") == 0) {
+				realClasses[nbTests] = 2;
+			}
+			else if (strcmp(movement, "sitDown") == 0) {
+				realClasses[nbTests] = 3;
+			}
+			else if (strcmp(movement, "standUp") == 0) {
+				realClasses[nbTests] = 4;
+			}
+			else if (strcmp(movement, "upstairs") == 0) {
+				realClasses[nbTests] = 5;
+			}
+			else if (strcmp(movement, "walking") == 0) {
+				realClasses[nbTests] = 6;
+			}
+
+			for (iData = 0; iData < NB_DATA; iData++) {
+				sum += data[iData];
+			}
+
+			double average = sum / iData;
+			double stadardDeviation[600];
+			double minValueAvergage = 100;
+			double minValueAvgDifference = 100;
+			double minValueStd = 100;
+
+			for (int iClass = 0; iClass < NB_CLASSES; iClass++) {
+				double std[600];
+				double result = distanceEuclidienne(data, models, iClass);
+
+				if (result < minValueAvergage) {
+					minValueAvergage = result;
+					indicators[0] = iClass + 1;
+				}
+
+				for (iData = 0; iData < NB_DATA; iData++) {
+					std[iData] = sqrt(pow(data[iData] - models[iClass].averages[iData], 2));
+				}
+				
+				sum = 0;
+				for (iData = 0; iData < NB_DATA; iData++) {
+					sum += pow((data[iData] - models[iClass].standardDeviation[iData]), 2);
+				}
+
+				double resultStd = sqrt(sum);
+				double valueMinStd = 100;
+				if (resultStd < minValueStd) {
+					valueMinStd = resultStd;
+					indicators[1] = iClass + 1;
+				}
+
+
+				double differenceAvg;
+				if (average < models[iClass].average) {
+					differenceAvg = models[iClass].average - average;
+				} else {
+					differenceAvg = average - models[iClass].average;
+				}
+
+				if (differenceAvg < minValueAvgDifference) {
+					minValueAvgDifference = differenceAvg;
+					indicators[2] = iClass + 1;
+				}
+			}
+
+			if (indicators[0] == indicators[1] || indicators[0] == indicators[2]) {
+				estimatedClasses[nbTests] = indicators[0];
+			} else {
+				if (indicators[1] == indicators[2]) {
+					estimatedClasses[nbTests] = indicators[1];
+				} else {
+					estimatedClasses[nbTests] = indicators[0];
+				}
+			}
+
+			nbTests++;
+		}
+
+		fclose(fiTest);
+
+		displayResultsByClass(realClasses, estimatedClasses, nbTests);
+		displayAccuracy(realClasses, estimatedClasses, nbTests);
+		displayConfusionMatrix(realClasses, estimatedClasses, nbTests);
+	}
+}
+
+double distanceEuclidienne(double data[], Model models[], int iClass) {
+	double result = 0;
+	int iData = 0;
+
+	while (iData < NB_DATA)
+	{
+		result += pow(data[iData] - models[iClass].averages[iData], 2);
+		iData++;
+	}
+	return result = sqrt(result);
 }
